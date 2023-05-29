57e25ebd35af7cb52998356861642036351671d8
Rohit Thakur <ThakurRohitDSingh@gmail.com>
2023-05-29 21:15:19 +0530

    added lambok jar to sts changed the annotations in NRFFGIE class added mysql 8 connector dependency in pom.xml

--- src/main/java/com/rs/baseproject/custom/exception/NoRecordFoundForGivenIdException.java
+++ src/main/java/com/rs/baseproject/custom/exception/NoRecordFoundForGivenIdException.java
@@ -4,16 +4,18 @@ package com.rs.baseproject.custom.exception;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-
 @Setter
+@Getter
+@ToString
 public class NoRecordFoundForGivenIdException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
